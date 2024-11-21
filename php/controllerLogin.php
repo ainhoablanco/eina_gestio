b0567e04112a33e47c7cbc74cb9a1270b3bd1f70
ainhoablanco <ablancoe2324@politecnics.barcelona>
2024-11-21 20:04:44 +0100

    canvis

--- php/controllerLogin.php
+++ php/controllerLogin.php
@@ -4,7 +4,7 @@ require_once('connexioBD.php');
 
 if (isset($_POST['select'])) {
     if (selectBD($_POST['name'], $_POST['password'])) {
-        header('Location: ../htdocs/tasks.html');
+        header('Location: ../htdocs/proyectos.html');
         exit();
     } else {
         echo "Nom d'usuari o contrasenya incorrectes";
