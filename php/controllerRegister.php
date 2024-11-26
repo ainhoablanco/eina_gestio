04c7e851268ea33339f7962930fae9f1dab248e7
ainhoablanco <ablancoe2324@politecnics.barcelona>
2024-11-26 18:38:37 +0100

    canvis

--- php/controllerRegister.php
+++ php/controllerRegister.php
@@ -5,7 +5,7 @@ require_once('connexioBD.php');
 if (isset($_POST['insert'])) {
     insertarBD($_POST['name'],$_POST['email'],$_POST['password']);
     
-    header('Location: ../htdocs/tasks.html');
+    header('Location: ../htdocs/proyectos.html');
     exit();
 }
 
